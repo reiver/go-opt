266d3f6da44a8ccda6fdca70a6b97fd4d35721f6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-05-18 11:24:46 -0700

    added docs

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package opt has a number of sub-packages provides alternatives to Go's built-in types (bool, float64, int64, string, time.Time)
+that also let you express a "lack of a value"; for the Go programming language; these are similar to "option types" or "maybe types"
+in some other programming languages.
+*/
+package opt
+
