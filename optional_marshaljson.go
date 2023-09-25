3e7cdb405e4f347bc1e93ed159e7bd9828e7d15b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-25 11:35:13 +0900

    .MarshalJSON()

--- optional_marshaljson.go
+++ optional_marshaljson.go
@@ -11,7 +11,7 @@ var _ json.Marshaler = Nothing[string]()
 // MarshalJSON makes it so json.Marshaler is implemented.
 func (receiver Optional[T]) MarshalJSON() ([]byte, error) {
 	switch interface{}(receiver.value).(type) {
-	case bool, string:
+	case bool, string, json.Marshaler:
 		// these are OK.
 	default:
 		return nil, fmt.Errorf("cannot marshal something of type %T into JSON because parameterized type is ‘%T’ rather than ‘bool’ or ‘string’", receiver, receiver.value)
